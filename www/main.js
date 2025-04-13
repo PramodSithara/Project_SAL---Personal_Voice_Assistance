175e73ed382d651f85b83e5acd81d6c0b0b1c84c
pramodsithara <pramodsithara9999@gmail.com>
2025-04-13 18:53:03 +0530

    Update listening functions

--- www/main.js
+++ www/main.js
@@ -10,4 +10,37 @@ $(document).ready(function () {
             effect: "bounceOut",
         },
     });
+
+    //siri wave
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 640,
+        height: 200,
+        style: "ios9",
+        amplitude: "1",
+        speed: "0.30",
+        autostart: true
+    });
+
+    //siri message animation
+    $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "fadeInUp",
+            sync: true,
+        },
+        out: {
+            effect: "fadeOutUp",
+            sync: true,
+        },
+    });
+
+    //mic button click event
+    $("#MicBtn").click(function (e) { 
+        eel.playAssistantSound();
+        $("#Oval").attr("hidden", true);
+        $("#SiriWave").attr("hidden", false);
+        eel.allCommands()()
+    });
 });
